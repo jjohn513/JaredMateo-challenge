4b01f0b87ff0621a786bb317c5a61a274871b74c
Jared Johnson <jared.johnson05@gmail.com>
2020-11-20 18:49:12 -0500

    updating challenges

--- medium.js
+++ medium.js
@@ -29,6 +29,30 @@
 
 // console.log(vowelChecker(input))
 
+/////////////////////////////////////////////////////////
+//////////////////////// Part 2 /////////////////////////
+
+let c = "So dark the con of man";
+let d = "Madonna of the Rocks";
+
+var a = c.toLowerCase();
+var b = d.toLowerCase();
+
+function compare(a, b) {
+  var y = a.replace(/\s/g, "");
+  var z = b.replace(/\s/g, "");
+
+  var sortY = y.split("").sort().join("");
+  var sortZ = z.split("").sort().join("");
+
+  if (sortY === sortZ) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+compare(a, b);
 
 /////////////////////////////////////////////////////////
 //////////////////////// Part 3 /////////////////////////
